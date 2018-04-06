d09ed373a6f69c33cdd2a167773f215288ca84bd
Ray Blasko <mray6288@gmail.com>
2018-04-06 16:55:44 -0400

    Done.

--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,18 @@
-class Spaceship {}
+class Spaceship {
+	constructor(name, crewMembers, phasers, shields){
+		this.name = name
+		this.phasers = phasers
+		this.shields = shields
+		this.cloaked = false
+		this.warpDrive = 'disengaged'
+		this.crew = crewMembers
+		crewMembers.length > 0 ? this.docked = false : this.docked = true
+		this.phasersCharge = 'uncharged'
+
+		crewMembers.forEach(member => member.currentShip = this)
+
+	}
+
+
+
+}
